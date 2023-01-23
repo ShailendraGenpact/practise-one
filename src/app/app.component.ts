2c36f74d82a894ca34835f55caeeb8925dd19d17
ShailendraGenpact <123370183+ShailendraGenpact@users.noreply.github.com>
2023-01-23 17:58:28 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,52 @@ export class AppComponent  {
 
   constructor(){
     this.fun1();
+    this.fun2();
+    this.fun3();
+  }
+
+  fun3(){
+    // let one = {id : 1 };
+    // let two = {name : "def"};
+
+    // let three = Object.assign(two, one);
+    // console.log("######", three);
+    // three.id = 11111;
+    // three.name = "hkjhkh"
+    // console.log("$$$$$$$$$$", one);
+    // console.log("%%%%%%%%%%%", two);
+
+  }
+
+  fun2(){ // not deep clone
+
+    let one = {id : 1,  add : "ghaziabad"};
+    let two = {name : "def"};
+
+    let three = {...one, ...two};
+    console.log("@@@@@@@@@", three);
+
+    three.id = 76;
+    three.name ="shailendra";
+    one.id = 999;
+    two.name ="hhhhh";
+
+    console.log("#########", three);
+
+    console.log("one", one);
+    console.log("two", two)
   }
 
   fun1(){
-    let a = [1,3,5,7,9,2,4,6,1,3,5,6,7,8];
-    let b = [];
+    // let a = [1,3,5,7,9,2,4,6,1,3,5,6,7,8];
+    // let b = [];
 
-    a.forEach(e => {
-      if(b.indexOf(e) == -1){
-        b.push(e);
-      }
-    })
+    // a.forEach(e => {
+    //   if(b.indexOf(e) == -1){
+    //     b.push(e);
+    //   }
+    // })
 
-    console.log("2222222 ", b)
+    // console.log("2222222 ", b)
   }
 }
